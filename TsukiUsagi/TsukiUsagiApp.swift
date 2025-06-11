9f8f30f569b5c4edcc63a3b6f241d5a400e63352
azu-azu <azu-azu@users.noreply.github.com>
2025-06-11 18:03:50 +0900

    Initial Commit

---
+++ TsukiUsagi/TsukiUsagiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TsukiUsagiApp.swift
+//  TsukiUsagi
+//
+//  Created by 松本和実 on 2025/06/11.
+//
+
+import SwiftUI
+
+@main
+struct TsukiUsagiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TsukiUsagiTests/TsukiUsagiTests.swift
@@ -0,0 +1,17 @@
+//
+//  TsukiUsagiTests.swift
+//  TsukiUsagiTests
+//
+//  Created by 松本和実 on 2025/06/11.
+//
+
+import Testing
+@testable import TsukiUsagi
+
+struct TsukiUsagiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
